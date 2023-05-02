589d7399ac36f501d847287a952cf2993e7ed824
Johan711024 <hyperjohan@hotmail.com>
2023-05-02 10:47:46 +0200

    minor fixes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,50 @@
+import { useState } from 'react';
 import EventForm from './components/EventForm';
 
+
 function App() {
+  const [isSubmitting, setIsSubmitting] = useState(true)
+  const [didSubmit, setDidSubmit] = useState(false)
+
+  const [submitSeed, setSubmitSeed] = useState(true)
+  const [eventIdSeed, setEventIdSeed] = useState(0)
+  
+  const url = 'https://ptseventsapp.azurewebsites.net/api'
+  
+  //Seed data
+  const seedData = {
+    "name": "5G mayhem",
+    "isComplete": false,      
+  }
+  
+  const submitHandler = async (apiController, submitData) => {
+    setSubmitSeed(false)
+    setIsSubmitting(true);
+    const response = await fetch(`${url}/${apiController}`, {
+      method: 'POST',
+      body: JSON.stringify(
+        submitData      
+      ),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    setIsSubmitting(false)
+    setDidSubmit(true)
+    const data = await response.json()
+    setEventIdSeed(data.id)
+    console.log(data)
+  }
+
+  submitSeed && submitHandler('PtsEvent', seedData)
+
   return (
+    <>
+    {isSubmitting && <p>Sending request...</p>}
     <div className="app">
-      <EventForm />
+      <EventForm onSubmitting={submitHandler} eventId={eventIdSeed} />
     </div>
+    </>
   )
 }
 
